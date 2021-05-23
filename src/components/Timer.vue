2a164db959266c86ab07812d5a37e5821686cfb4
potdig <potadig.ashnerd.zino@gmail.com>
2021-05-23 18:49:49 +0900

    format time

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <p>{{ time }}</p>
+    <p>{{ formattedTime.slice(0, -1) }}</p>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { Duration } from 'luxon'
 import timer from '../lib/timer'
 
 timer.init()
+
+const formatForSeconds = 's.SSS'
+const formatForMinutes = 'm:ss.SSS'
+const formatForHours = 'h:mm:ss.SSS'
 export default {
   computed: {
-    ...mapGetters(['time'])
+    ...mapGetters(['time']),
+    formattedTime() {
+      const duration = Duration
+        .fromMillis(this.time)
+        .shiftTo('hours', 'minutes', 'seconds', 'milliseconds')
+      if (duration.hours) {
+        return duration.toFormat(formatForHours)
+      } else if (duration.minutes) {
+        return duration.toFormat(formatForMinutes)
+      } else {
+        return duration.toFormat(formatForSeconds)
+      }
+    }
   }
 }
 </script>
