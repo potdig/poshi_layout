08021cf797c6579044e649c5d92602c0d5002e05
potdig <potadig.ashnerd.zino@gmail.com>
2021-05-24 06:33:32 +0900

    add game to store

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,21 +1,29 @@
 import { createStore } from 'vuex'
 import { Duration } from 'luxon'
+import Game from './game'
 
 export const store = createStore({
   state() {
     return {
-      time: 0
+      time: 0,
+      game: new Game()
     }
   },
   mutations: {
     updateTime(state, time) {
       state.time = time
+    },
+    updateGame(state, game) {
+      state.game = game
     }
   },
   getters: {
     time(state) {
       return Duration.fromMillis(state.time)
         .shiftTo('hours', 'minutes', 'seconds', 'milliseconds')
+    },
+    game(state) {
+      return state.game
     }
   }
 })
