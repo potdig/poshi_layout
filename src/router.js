1683ef601f1bc67dd9b5a73d242a4668f59b2179
potdig <potadig.ashnerd.zino@gmail.com>
2021-05-28 22:41:42 +0900

    change sidebar type with query

--- src/router.js
+++ src/router.js
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SideBar from './components/SideBar.vue'
 
 const routes = [
-  { path: '/', component: SideBar },
-  { path: '/casual', component: SideBar, props: { casual: true } },
-  { path: '/transparent', component: SideBar, props: { transparent: true } },
-  { path: '/casual_transparent', component: SideBar, props: { casual: true, transparent: true } }
+  {
+    path: '/',
+    component: SideBar,
+    props: ({ query }) => ({
+        casual: Boolean(query.casual),
+        transparent: Boolean(query.transparent)
+    })
+  }
 ]
 
 export default createRouter({
